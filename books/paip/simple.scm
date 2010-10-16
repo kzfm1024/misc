ffddb9269991a4b0d2ecec7b56c522d1f2a67016
kzfm1024 <kzfm1024@gmail.com>
2010-10-16 17:04:21 +0900

    simple.scm

--- books/paip/simple.scm
+++ books/paip/simple.scm
@@ -1,7 +1,25 @@
 (use srfi-1)
 (use srfi-27)
 
-(define rest cdr)
+;;
+;; Common Lisp 
+;;  (cdr '()) は NIL を返す。
+;;  assoc は一致しない場合は NIL を返す。
+;;  NIL は false として扱われる。
+;;
+;; Scheme 
+;;  (cdr '()) はエラーになる。
+;;  assoc は一致しない場合は #f を返す。
+;;  '() は true として扱われる。
+;;
+;; rest
+;; (rest #f) => #f
+;; (rest '()) => #f
+;;
+(define (rest lst)
+  (if (or (eq? lst #f) (null? lst))
+      #f
+      (cdr lst)))
 
 (define (elt lst n) (first (drop lst n)))
 
@@ -9,6 +27,14 @@
   (elt choices (random-integer (length choices))))
 
 (random-source-randomize! default-random-source)
+
+(define null null?)
+
+(define listp pair?)
+
+(define mappend append-map)
+
+(define mapcar map)
 
 ;;; ==============================
 
@@ -34,8 +60,58 @@
 ;;; ==============================
 
 (define (generate phrase)
-  (cond ((pair? phrase)
-         (append-map generate phrase))
+  (cond ((listp phrase)
+         (mappend generate phrase))
         ((rewrites phrase)
          (generate (random-elt (rewrites phrase))))
         (else (list phrase))))
+
+;;; ==============================
+
+(define *bigger-grammar*
+  '((sentence -> (noun-phrase verb-phrase))
+    (noun-phrase -> (Article Adj* Noun PP*) (Name) (Pronoun))
+    (verb-phrase -> (Verb noun-phrase PP*))
+    (PP* -> () (PP PP*))
+    (Adj* -> () (Adj Adj*))
+    (PP -> (Prep noun-phrase))
+    (Prep -> to in by with on)
+    (Adj -> big little blue green adiabatic)
+    (Article -> the a)
+    (Name -> Pat Kim Lee Terry Robin)
+    (Noun -> man ball woman table)
+    (Verb -> hit took saw liked)
+    (Pronoun -> he she it these those that)))
+
+;; (set! *grammar* *bigger-grammar*)
+
+;;; ==============================
+
+(define (generate-tree phrase)
+  (cond ((listp phrase)
+         (mapcar generate-tree phrase))
+        ((rewrites phrase)
+         (cons phrase
+               (generate-tree (random-elt (rewrites phrase)))))
+        (else (list phrase))))
+
+;;; ==============================
+
+(define (generate-all phrase)
+  (cond ((null phrase) (list nil))
+        ((listp phrase)
+         (combine-all (generate-all (first phrase))
+                      (generate-all (rest phrase))))
+        ((rewrites phrase)
+         (mappend generate-all (rewrites phrase)))
+        (else (list (list phrase)))))
+
+#|
+  "Return a list of lists formed by appending a y to an x.
+  E.g., (combine-all '((a) (b)) '((1) (2)))
+  -> ((A 1) (B 1) (A 2) (B 2))."
+|#
+(define (combine-all xlist ylist)
+  (mappend (lambda (y)
+             (mapcar (lambda (x) (append x y)) xlist))
+           ylist))
