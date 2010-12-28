00e3356d0598f37804a94875300092d869dda276
kzfm1024 <kzfm1024@gmail.com>
2010-12-29 00:45:07 +0900

    bingo.scm

--- bin/bingo.scm
+++ bin/bingo.scm
@@ -1,4 +1,5 @@
 (use srfi-1)
+(use srfi-27)
 (use gauche.sequence)
 
 (define (bingo-numbers)
@@ -8,5 +9,7 @@
         (G (shuffle (iota 15 46)))
         (O (shuffle (iota 15 60))))
     (map list B I N G O)))
+
+(random-source-randomize! default-random-source)
 
 (for-each (lambda (l) (print l)) (bingo-numbers))
