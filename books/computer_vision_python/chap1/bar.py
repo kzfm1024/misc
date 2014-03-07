bc5b77d31ec2f15712e5b23d19d3c7f3f8ebafcd
Kazufumi Oyama <Kazufumi.Oyama@jp.sony.com>
2014-03-07 21:44:48 +0900

    added computer_vision_python

---
+++ books/computer_vision_python/chap1/bar.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from pylab import *
+
+im = array(Image.open('baboon.jpg').convert('L'))
+figure()
+gray()
+contour(im, origin='image')
+axis('equal')
+axis('off')
+figure()
+hist(im.flatten(), 128)
+show()
+
+
+
