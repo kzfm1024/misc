926fe8ccfecc4dcb26cf17febb05747dae3038f4
Kazufumi Cho <kzfm1024@gmail.com>
2015-07-21 07:53:44 +0900

    added CMakeLists.txt

--- books/IO3DCGGLSL/chap3/SL_TextureOSR/slTextureOSR.cpp
+++ books/IO3DCGGLSL/chap3/SL_TextureOSR/slTextureOSR.cpp
@@ -8,13 +8,14 @@ Microsoft Visual Studio .NET2005 Standard Edition
 off-screen rendering によるテクスチャ作成
 OpenGL+GLSL
 */
-#include <windows.h>
+//#include <windows.h>
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include "../../myPrimitive2.h"
 #include "../../myTexture2.h"
 #include "../../myGlsl.h"
+#include <time.h> // for clock_gettime()
 
 //関数のプロトタイプ宣言
 void init();
@@ -197,6 +198,15 @@ void setTexture(void)
   glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);//NEAREST);
   glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);//NEAREST);
   glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+static double timeGetTime()
+{
+	struct timespec tp;
+	clock_gettime(CLOCK_REALTIME, &tp);
+	double msec = (double)tp.tv_sec * 1000.0 + (double)tp.tv_nsec / 1000.0;
+	printf("%s: msec %f\n", __FUNCTION__, msec);
+	return msec;
 }
 
 void display(void)
