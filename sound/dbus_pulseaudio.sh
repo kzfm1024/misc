04eaaaed64151d0d2870cf43e4a2b14e9029005e
Kazufumi Cho <kzfm1024@gmail.com>
2016-09-19 18:02:54 +0900

    changed a little

--- sound/dbus_pulseaudio.sh
+++ sound/dbus_pulseaudio.sh
@@ -1,10 +1,6 @@
 
 dbus-send --print-reply --dest=org.PulseAudio1 /org/pulseaudio/server_lookup1 org.freedesktop.DBus.Properties.Get string:'org.PulseAudio.ServerLookup1' string:'Address'
 
-dbus-send --address=unix:path\=/run/user/1000/pulse/dbus-socket --print-reply /org/pulseaudio/core1 org.freedesktop.DBus.Introspectable.Introspect
-
-dbus-send --address=unix:path\=/run/user/1000/pulse/dbus-socket --print-reply /org/pulseaudio/core1/sink0 org.freedesktop.DBus.Introspectable.Introspect
-
 #
 # /org/pulseaudio/core1
 #
