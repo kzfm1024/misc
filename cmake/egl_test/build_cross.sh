97dae058c3fdb8c9a959c8ddf70e4a9f06a2c229
Kazufumi Cho <kzfm1024@gmail.com>
2017-02-04 10:30:35 +0900

    add egl_test

---
+++ cmake/egl_test/build_cross.sh
@@ -0,0 +1,9 @@
+TOOLCHAIN_FILE=`pwd`/rostoolchain-aarch64.cmake
+
+mkdir -p build_cross
+(cd build_cross; \
+ cmake -G"Eclipse CDT4 - Unix Makefiles" \
+       -DCMAKE_BUILD_TYPE=Debug \
+       -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN_FILE \
+       ../src; \
+ make)
