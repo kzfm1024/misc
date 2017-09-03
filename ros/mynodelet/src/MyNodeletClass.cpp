4e96c40a564fcac7545344e445f8133620e1ae1f
Kazufumi Cho <kzfm1024@gmail.com>
2017-09-03 09:43:03 +0900

    add mynodelet

---
+++ ros/mynodelet/src/MyNodeletClass.cpp
@@ -0,0 +1,15 @@
+// this should really be in the implementation (.cpp file)
+#include <pluginlib/class_list_macros.h>
+#include <mynodelet/MyNodeletClass.h>
+
+// watch the capitalization carefully
+PLUGINLIB_EXPORT_CLASS(example_pkg::MyNodeletClass, nodelet::Nodelet)
+
+namespace example_pkg
+{
+    void MyNodeletClass::onInit()
+    {
+        NODELET_DEBUG("Initializing nodelet...");
+        ROS_INFO("MyNodeletClass::onInit");
+    }
+}
