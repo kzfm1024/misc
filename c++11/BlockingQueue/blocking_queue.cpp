63591262db71c3df5f78a37d5c315d22e7f850be
Kazufumi Cho <kzfm1024@gmail.com>
2017-09-10 13:34:58 +0900

    add thread test

--- c++11/BlockingQueue/blocking_queue.cpp
+++ c++11/BlockingQueue/blocking_queue.cpp
@@ -1,24 +1,58 @@
 #include <iostream>
 #include <thread>
+#include <atomic>
+#include <cassert>
 #include "BlockingQueue.h"
+#include <sched.h>
+
+std::atomic<int> s_flag;
+BlockingQueue<int> s_que(1);
+
+int set_sched_priority(int policy, int prio)
+{
+    struct sched_param param;
+    sched_getparam(0, &param);
+    int prio1 = param.sched_priority;
+    
+    param.sched_priority = prio;
+    int ret =  sched_setscheduler(0, policy, &param);
+    assert(ret == 0);
+    
+    sched_getparam(0, &param);
+    int prio2 = param.sched_priority;
+    std::cout << "set_sched_priority:" << prio1 << "->" << prio2 << std::endl;
+    
+    return ret;
+}
+
+void producer()
+{
+    set_sched_priority(SCHED_FIFO, 56);
+    
+    for (int i = 0; i < 10; i++)
+    {
+        s_que.put(i);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    s_flag = 0;
+    s_que.put(-1);
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+}
 
 int main()
 {
-    BlockingQueue<int> que(8);
+    set_sched_priority(SCHED_FIFO, 55);
     
-    que.put(1);
-    que.put(2);
-    que.put(3);
-    que.put(4);
-    que.put(5);
-    que.clear();
-    que.put(6);
-    que.put(7);
-    que.put(8);
+    std::thread th(producer);
+    s_flag = 1;
 
-    while (!que.empty())
+    while (s_flag)
     {
-        std::cout << que.take() << " ";
+        std::cout << s_que.take() << std::endl;
     }
-    std::cout << std::endl;
+
+    std::cout << "waiting for producer thread..." << std::endl;
+    th.join();
 }
